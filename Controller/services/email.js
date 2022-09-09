afb016cae02b78eb8c877abfd8e3c06c4e3ac149
C0618C <leopardus@qq.com>
2022-09-10 01:12:12 +0800

    完善发邮件接口

--- Controller/services/email.js
+++ Controller/services/email.js
@@ -8,8 +8,6 @@ const ApiResponse = require("./../../Entity/ApiResponse");
 const Models = require("./../../Core/OTO/Models");
 
 
-let mailServerSetting = {}; //TODO: 数据库读取
-
 const EMAIL_SETTING_GROUP = "send_email_account";       //系统设置-邮箱的配置分组
 const KINDLE_INBOX = "kindle_inbox";                   //系统设置-收件箱-可用于kindle的收件箱
 
@@ -50,21 +48,54 @@ function CreateTransport(user, pass) {
  * @param {{ title, content, files, mailto, sender, pass }} 邮件属性
  * @param {Array} files 文件要求相对地址数组，会自动拆分成系统接口所需格式:`{filename,path}` 或者 `{filename,content,contentType:'text/plain'}`指定文件格式
  */
-async function SendAMail({ title, content, files, mailto, sender, pass }) {
-    return new Promise((resolve, reject) => {
+async function SendAMail({ title, content, files, mailto = "", sender = "", pass = "" }) {
+    return new Promise(async (resolve, reject) => {
         try {
             // console.log("准备发送邮件：", title, content, files)
 
+            const myModels = new Models();
+
+            if (mailto === "") {
+                let mt = await myModels.SystemConfig.findOne({
+                    where: {
+                        Group: KINDLE_INBOX,
+                        Name: "address"
+                    }
+                });
+                if (mt) mailto = mt.Value;
+            }
+            if (sender === "") {
+                let mt = await myModels.SystemConfig.findOne({
+                    where: {
+                        Group: EMAIL_SETTING_GROUP,
+                        Name: "address"
+                    }
+                });
+                if (mt) sender = mt.Value;
+                let pt = await myModels.SystemConfig.findOne({
+                    where: {
+                        Group: EMAIL_SETTING_GROUP,
+                        Name: "password"
+                    }
+                });
+                if (pt) pass = mt.Value;
+            }
+
+            if (sender === "" || mailto === "" || pass === "") {
+                reject("默认邮箱配置不完整，不能发送邮件，请先在系统设置收/发件信息；或直接指定收/发件信息。");
+                return;
+            }
+
             //邮件属性——邮件的详细信息
             var mailOptions = {
-                from: sender || mailServerSetting.email,
-                to: mailto || mailServerSetting.kindle_email,
-                subject: title || "Send the mail by default",
+                from: sender,
+                to: mailto,
+                subject: title || "Send the e-mail by default",
                 text: content || "This e-mail sent by EBookWorkshop!",
-                attachments: []     //附件地址，需要绝对地址
+                attachments: []     //附件地址，需要相对地址
             };
 
-            //添加附件
+            //添加附件-将附件按插件发送要求转换
             if (files) {        //NOTE: 这儿会有将服务器任意文件通过邮件发出去的bug，会泄露服务器信息。
                 //mailOptions.attachments = files.concat();
                 for (let f of files) {
@@ -102,7 +133,7 @@ module.exports = () => ({
      *     tags:
      *       - Services - EMail —— 系统服务：邮件
      *     summary: 通过简易的SMTP服务发送邮件
-     *     description: 可以发一封邮件，能带附件
+     *     description: 可以发一封邮件，能带附件，如果没有发件人/收件人信息，则默认从系统配置里读取
      *     parameters:
      *       - in: body
      *         name: email
