028531b83ef03f4cbd5ea5623c7434fd77804540
C0618C <leopardus@qq.com>
2025-04-04 00:47:45 +0800

    修正网文书对象化章节标题出错问题

--- Core/OTO/DO/WebBook.js
+++ Core/OTO/DO/WebBook.js
@@ -203,7 +203,7 @@ class OTO_WebBook {
                 });
                 let tIdx = new WebIndex({ ...i.dataValues, ...eI?.dataValues, curHost: defaultHost, HasContent: i.HasContent });
                 for (let u of eI.WebBookIndexURLs) tIdx.URL.push({ id: u.id, Path: u.Path });
-                [tIdx.Title] = Reviewer(ebookObj.ReviewRules, [tIdx.WebTitle])
+                [tIdx.Title] = Reviewer(ebookObj.ReviewRules, [tIdx.Title])
 
                 webBook.Index.push(tIdx);
             }
